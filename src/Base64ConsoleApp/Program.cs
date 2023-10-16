4f01a3a48464b77eda434c27ce7fe20d6aa133b1
armameje <armamentojenisi@gmail.com>
2023-10-16 13:37:22 +0800

    Add control flow for input commdanLine args

--- src/Base64ConsoleApp/Program.cs
+++ src/Base64ConsoleApp/Program.cs
@@ -2,3 +2,26 @@
 
 StringEncrypterService stringEncrypterService = new();
 
+try
+{
+    var method = args[0]; // Enter either "e" or "d" to determine whether to encrypt or decrypt respectively
+    var stringToEncryptOrDecrypt = args[1];
+
+    Console.WriteLine($"Input string: {stringToEncryptOrDecrypt}");
+
+    switch (method)
+    {
+        case "e":
+            Console.WriteLine($"Encrypted string: {stringEncrypterService.EncryptDecode("e", stringToEncryptOrDecrypt)}");
+            break;
+        case "d":
+            Console.WriteLine($"Decrypted string: {stringEncrypterService.EncryptDecode("d", stringToEncryptOrDecrypt)}");
+            break;
+        default:
+            throw new Exception();
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
