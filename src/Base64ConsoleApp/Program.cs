86514599f2d76750b990b34ff3534de39b9133af
armameje <armamentojenisi@gmail.com>
2023-10-16 13:03:37 +0800

    Add encrypter service to encode and decode string to and from Base64

---
+++ src/Base64ConsoleApp/Program.cs
@@ -0,0 +1,4 @@
+﻿using StringEncrypter;
+
+StringEncrypterService stringEncrypterService = new();
+
---
+++ src/Base64ConsoleApp/StringEncrypterService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringEncrypter
+{
+    internal class StringEncrypterService
+    {
+        private string EncodeToBase64(string input)
+        {
+            var inputBytes = Encoding.UTF8.GetBytes(input);
+
+            return Convert.ToBase64String(inputBytes);
+        }
+
+        private string DecodeToString(string input)
+        {
+            var inputBytes = Convert.FromBase64String(input);
+
+            return Encoding.UTF8.GetString(inputBytes);
+        }
+
+        public string EncryptDecode(string method, string input) => method switch
+        {
+            "e" => EncodeToBase64(input),
+            "d" => DecodeToString(input),
+            _ => throw new Exception()
+        };
+    }
+}
